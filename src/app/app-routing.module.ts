397d9ee0e81febd00d3a0e2fb53f3e42bc1ce60e
bwilm <32461222+bwilm@users.noreply.github.com>
2019-04-25 15:35:44 -0400

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccomodationComponent } from 'src/app/accomodation/accomodation.component';
+import { CelebrationsComponent } from 'src/app/celebrations/celebrations.component';
+import { FoodAndBeverageComponent } from 'src/app/food-and-beverage/food-and-beverage';
+import { LifestyleComponent } from 'src/app/lifestyle/lifestyle.component';
+import { WeddingsComponent } from 'src/app/weddings/weddings.component';
+import { OffersComponent } from 'src/app/offers/offers.component';
+import { SigninComponent } from 'src/app/auth/signin/signin.component';
+import { SignupComponent } from 'src/app/auth/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'accomodation', component: AccomodationComponent},
+  { path: 'celebrations', component: CelebrationsComponent},
+  { path: 'food-and-beverage', component: FoodAndBeverageComponent},
+  { path: 'lifestyle', component: LifestyleComponent},
+  { path: 'weddings', component: WeddingsComponent},
+  { path: 'offers', component: OffersComponent},
+  { path: 'signin', component: SigninComponent},
+  { path: 'signup', component: SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/food-and-beverage/food-and-beverage.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-food-and-beverage',
+  templateUrl: './food-and-beverage.html',
+  styleUrls: ['./food-and-beverage.css']
+})
+export class FoodAndBeverageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
