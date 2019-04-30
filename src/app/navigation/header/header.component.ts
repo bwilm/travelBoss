47f0301fe7fb6ef0f8f17350bb4acebf4994c63f
bwilm <32461222+bwilm@users.noreply.github.com>
2019-04-29 22:03:54 -0400

    added scss files etc

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { emit } from 'cluster';
+
 import { SidenavListComponent } from 'src/app/navigation/sidenav-list/sidenav-list.component';
 
 @Component({
